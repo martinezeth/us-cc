f7b90f6210c5d0047c2bd046ee132d30dc46e5c5
Nic Randazzo <nicmrandazzo@gmail.com>
2024-03-25 21:16:39 -0700

    wireframe1

--- us-cc/src/App.js
+++ us-cc/src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
-import './App.css';
+import bodyComponent from '../src/Components/bodyComponent'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="window">
+      
+      <div className="headerContainer">
+        <div className="headerImage"></div>
+        <div className="headerMenu"></div>
+        <div className="accountContainer"></div>
+      </div>
+      
+      <div className="bodyContainer">
+        <bodyComponent />
+      </div>
+      
+      <div className="footerContainer">
+        <div className="footerSection"></div>
+        <div className="footerSection"></div>
+        <div className="footerSection"></div>
+      </div>      
+    
     </div>
   );
 }
---
+++ us-cc/src/Components/bodyComponent.js
@@ -0,0 +1,13 @@
+
+
+export default function bodyComponent(){
+
+    return(
+        <>
+            <div className="background"></div>
+            <div className="bodyMenu1"></div>
+            <div className="bodyMenu2"></div>
+            <div className="bodyMenu3"></div>
+        </>
+    );
+}
