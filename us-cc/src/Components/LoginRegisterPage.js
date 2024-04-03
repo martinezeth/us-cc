52185af9211aad2c76ab87af1d5d37576671db0b
Nic Randazzo <nicmrandazzo@gmail.com>
2024-04-03 01:12:23 -0700

    started the create a post page wireframe.. need to learn about MUI components so I can work faster

--- us-cc/src/Components/LoginRegisterPage.js
+++ us-cc/src/Components/LoginRegisterPage.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import HeaderComponent from "./HeaderComponent";
 import { Button, Container } from "@mui/material";
 
 function Register() {
---
+++ us-cc/src/Components/NewReportPage.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { Button, Container, FormControl, FormGroup } from "@mui/material";
+
+export default function NewReportPage(){
+    
+    return (
+        <Container>
+            {/* This box is the "REPORTS NEARBY" area */}
+            <Box>
+            </Box>
+            {/* This box is where they write the title and body of the post */}
+            <Box>
+                <div>
+
+                </div>
+                <div>
+                    <FormGroup>
+                        <FormControl></FormControl>
+                        <FormControl></FormControl>
+                    </FormGroup>
+                </div>
+            </Box>
+
+        </Container>
+    );
+
+}
