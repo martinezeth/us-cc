304ec993b5082fd61dddf32f48ac9d05700b46a7
GGDoctor <127359046+GGDoctor@users.noreply.github.com>
2024-04-17 11:26:37 -0700

    Fixed Image
    Fixed Landing Page Image not loading. Moving onto sub-fixed and other information.

--- us-cc/src/Pages/LandingPage.js
+++ us-cc/src/Pages/LandingPage.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Box, VStack, Text, Image } from '@chakra-ui/react';
+import { Box, VStack, Text, Image, Img } from '@chakra-ui/react';
 import AboutSection from '../Components/AboutSection';
 import FeaturesSection from '../Components/FeaturesSection';
+import mainImage from '../Images/landingPageMainImage.png';
+import fallback from '../Images/landingPageFallback.png';
 
 
 export default function LandingPage() {
   return (
     <Box p={5} bg="gray.100">
       <VStack spacing={8}>
-        <Text fontSize="2xl">Welcome to United States Crisis Coordination</Text>
-        <Image src="../Images/landingPageMainImage.webp" alt="Main Banner" />
+        <Text fontSize="4xl">Welcome to United States Crisis Coordination</Text>
+        <Box>
+          <Img boxSize='600px' width='100%' src={mainImage} fallbackSrc={fallback}  alt='Main Banner' />
+        </Box>
         <AboutSection />
         <FeaturesSection />
       </VStack>
