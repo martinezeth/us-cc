6c6c9e72f7fc53dcb61f1b28adb170a4b94007a9
GGDoctor <127359046+GGDoctor@users.noreply.github.com>
2024-04-21 22:07:55 -0700

    Fixed Dropdown/Header

--- us-cc/src/Components/Disasters.js
+++ us-cc/src/Components/Disasters.js
@@ -1,15 +1,43 @@
-import React from "react";
-import {Select} from '@chakra-ui/react';
+// import React from "react";
+// import {Button, VStack, Select} from '@chakra-ui/react'
+// import {Link} from 'react-router-dom';
 
-export default function DropDown(){
-    return(
-        <Select  placeholder="Disasters">
-            <option value='wildfire'>Wildfire</option>
-            <option value='flood'>Flood</option>
-            <option value='hurricane'>Hurricane</option>
-            <option value='earthquake'>Earthquake</option>
-            <option value='virus'>Virus</option>
-            <option value='zombies'>Zombies</option>
-        </Select>
+// export default function DropDown(){
+    
+//     return(
+//         <VStack>
+//         <Select  placeholder="Disasters">
+//             <Button  as={Link} to="/wildfire">Wildfire</Button>
+//             <Button  as={Link} to="/flood">Flood</Button>
+//             <Button  as={Link} to="/hurricane">Hurricane</Button>
+//             <Button  as={Link} to="/earthquake">Earthquake</Button>
+//             <Button  as={Link} to="/COVID">COIVD-19</Button>
+//         </Select>
+//         </VStack>
+//     );
+// };
+
+import React from "react";
+import { VStack, Menu, MenuButton, MenuList, MenuItem, Button } from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import { Link } from "react-router-dom";
+
+export default function DropDown() {
+    return (
+      <VStack>
+        <Menu borderWidth="0px">
+          <MenuButton as={Button} rightIcon={<ChevronDownIcon />} bg='blue.600'>
+            Disasters
+          </MenuButton>
+          <MenuList borderColor="blue.600" padding={0} fontStyle="bold">
+            <MenuItem as={Link} to="/wildfire" bg='blue.600' color="black">Wildfire</MenuItem>
+            <MenuItem as={Link} to="/flood" bg='blue.600' color="black">Flood</MenuItem>
+            <MenuItem as={Link} to="/hurricane" bg='blue.600' color="black">Hurricane</MenuItem>
+            <MenuItem as={Link} to="/earthquake" bg='blue.600'color="black">Earthquake</MenuItem>
+            <MenuItem as={Link} to="/COVID" bg='blue.600' color="black">COVID-19</MenuItem>
+          </MenuList>
+        </Menu>
+      </VStack>
     );
-};
+  }
+  
