6f4cccf4ce93ed717a8c59082b07a0eb8fd66ed6
GGDoctor <127359046+GGDoctor@users.noreply.github.com>
2024-04-22 11:16:58 -0700

    Updated pages with images. Added images.

--- us-cc/src/Pages/Earthquake.js
+++ us-cc/src/Pages/Earthquake.js
@@ -1,15 +1,40 @@
 import * as React from 'react';
+import {VStack, Box, Text, useColorModeValue} from '@chakra-ui/react'
 import { useLocation } from 'react-router-dom';
 import CssBaseline from '@mui/material/CssBaseline';
-import { CardContent, Typography, Card, Box } from '@mui/material';
+import earthquake from '../Images/earthquake.png';
 
 
-export default function Earthquake() {
+export default function Wildfire() {
+    const bg = useColorModeValue('gray.50', 'gray.800');
+    const textColor = useColorModeValue('gray.800', 'gray.100');
+
     return(
         <>
-            <p>
-                Hello Earthquake
-                </p>        
-        </>
+        <Box
+            w="full"
+            h="60vh"
+            m={0}
+            p={0}
+            bg={bg}
+            align="center"
+            justify="center"
+            bgImage={`url(${earthquake})`}
+            bgPos="center"
+            bgSize="cover"
+        >
+        </Box>
+        <VStack
+         spacing={8}
+         justify="center"
+         align="center"
+         height="full"
+         bgGradient="linear(to-r, blackAlpha.600, transparent)"
+         >
+            <Text fontSize='4xl' color={textColor} textAlign='left' maxW="lg">
+                Earthquake
+            </Text>
+        </VStack>
+       </>
     )
 };
