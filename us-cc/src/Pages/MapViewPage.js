815a0383e15f7f7049fcaa7e4102778a1dced4bf
martinezeth <emartinez34@att.net>
2024-04-29 15:23:32 -0700

    Huge progress. Finally events are showing up on the map! Still need to fix visibility of the icons themselves on the map to make them stand out more.

--- us-cc/src/Pages/MapViewPage.js
+++ us-cc/src/Pages/MapViewPage.js
@@ -1,124 +1,50 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
-import L, { icon } from 'leaflet';
-import { Box, ChakraProvider } from '@chakra-ui/react'
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import L from 'leaflet';
+import { Box, ChakraProvider } from '@chakra-ui/react';
 import axios from 'axios';
 
-const AccidentIcon = L.Icon.extend({
+import accidentIconUrl from '../Images/Icons/accidentEventIcon.svg';
+import earthquakeIconUrl from '../Images/Icons/earthquakeEventIcon.svg';
+import fireIconUrl from '../Images/Icons/fireEventIcon.svg';
+import firstAidIconUrl from '../Images/Icons/firstAidEventIcon.svg';
+import floodIconUrl from '../Images/Icons/floodEventIcon.svg';
+import hailIconUrl from '../Images/Icons/hailEventIcon.svg';
+import highWindsIconUrl from '../Images/Icons/highWindEventIcon.svg';
+import blizzardIconUrl from '../Images/Icons/blizzardEventIcon.svg';
+import lightningIconUrl from '../Images/Icons/lightningEventIcon.svg';
+
+var baseIcon = L.Icon.extend({
   options: {
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    iconUrl: '../Images/Icons/accidentEventIcon.svg'
+    iconSize: [25,41],
+    iconAnchor: [12,41],
+    popupAnchor: [1,-34]
   }
 });
 
-const EarthquakeIcon = L.Icon.extend({
-  options: {
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    iconUrl: '../Images/Icons/earthquakeEventIcon.svg'
-  }
-});
+var accidentIcon = new baseIcon({iconUrl: accidentIconUrl}),
+    earthquakeIcon = new baseIcon({iconUrl: earthquakeIconUrl}),
+    fireIcon = new baseIcon({iconUrl: fireIconUrl}),
+    firstAidIcon = new baseIcon({iconUrl: firstAidIconUrl}),
+    floodIcon = new baseIcon({iconUrl: floodIconUrl}),
+    hailIcon = new baseIcon({iconUrl: hailIconUrl}),
+    highWindsIcon = new baseIcon({iconUrl: highWindsIconUrl}),
+    blizzardIcon = new baseIcon({iconUrl: blizzardIconUrl}),
+    lightningIcon = new baseIcon({iconUrl: lightningIconUrl});
 
-const FireIcon = L.Icon.extend({
-  options: {
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    iconUrl: '../Images/Icons/fireEventIcon.svg'
-  }
-});
 
-const FirstAidIcon = L.Icon.extend({
-  options: {
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    iconUrl: '../Images/Icons/firstAidEventIcon.svg'
-  }
-});
-
-const FloodIcon = L.Icon.extend({
-  options: {
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    iconUrl: '../Images/Icons/floodEventIcon.svg'
-  }
-});
-
-const HailIcon = L.Icon.extend({
-  options: {
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    iconUrl: '../Images/US-CC-header.png'
-    //iconUrl: '../Images/Icons/hailEventIcon.svg'
-  }
-});
-
-const HighWindsIcon = L.Icon.extend({
-  options: {
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    iconUrl: '../Images/Icons/highWindEventIcon.svg'
-  }
-});
-
-const BlizzardIcon = L.Icon.extend({
-  options: {
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    iconUrl: '../Images/Icons/blizzardEventIcon.svg'
-  }
-});
-
-const LightningIcon = L.Icon.extend({
-  options: {
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    iconUrl: '../Images/Icons/lightningEventIcon.svg'
-  }
-});
-
-const mapIcons = {
-  accident: new AccidentIcon(),
-  earthquake: new EarthquakeIcon(),
-  fire: new FireIcon(),
-  firstAid: new FirstAidIcon(),
-  flood: new FloodIcon(),
-  hail: new HailIcon(),
-  highWinds: new HighWindsIcon(),
-  blizzard: new BlizzardIcon(),
-  lightning: new LightningIcon(),
-};
-const singleIcon =
-  {
-    incident_type: "Type B", 
-    description: "Description of Type B", 
-  location_lat: 38.338075, 
-  location_lng: -122.675004
-  }
-function SingleIconMarker({ incident }) {
-  const { location_lat, location_lng, incident_type, description } = incident;
-
-  return (
-
-      <Marker position={[location_lat, location_lng]} icon={mapIcons['hail']}>
-        <Popup>
-          <strong>{incident_type}</strong>
-          <br />
-          {description}
-          <br />
-        </Popup>
-      </Marker>
-  );
-}
+  const mapIcons = {
+    accident: accidentIcon,
+    earthquake: earthquakeIcon,
+    fire: fireIcon,
+    firstAid: firstAidIcon,
+    flood: floodIcon,
+    hail: hailIcon,
+    highWinds: highWindsIcon,
+    blizzard: blizzardIcon,
+    lightning: lightningIcon,
+  };
+    
 
 function LocationMarker() {
   const [position, setPosition] = useState(null);
@@ -138,79 +64,71 @@ function LocationMarker() {
   );
 }
 
+function normalizeIncidentType(type) {
+  return type.toLowerCase().replace(/ /g, ''); // Convert to lowercase and remove spaces
+}
+
 function MapPage() {
   const [zoom, setZoom] = useState(13); // Default zoom level
   const mapRef = useRef(null);
   const [incidents, setIncidents] = useState([]);
 
-  // useEffect(() => {
-  //   axios.get('http://localhost:5000/api/incident-reports')
-  //     .then(response => {
-  //       console.log('Incidents fetched:', response.data); // DEBUG LINE
-  //       setIncidents([...incidents, ...response.data]);
-  //     })
-  //     .catch(error => 
-  //       console.error("Error fetching incident reports for map view:", error));
-  // }, []);
-
   useEffect(() => {
-    if (mapRef.current) {
-      mapRef.current.leafletElement.setZoom(zoom);
-    }
-  }, [zoom]);
+    axios.get('http://localhost:8000/api/incident-reports')
+      .then(response => {
+        console.log('Incidents fetched:', response.data); // DEBUG LINE
+        response.data.forEach(incident => {
+          const normalizedType = normalizeIncidentType(incident.incident_type);
+          if (!mapIcons[normalizedType]) {
+            console.error("No icon found for incident type:", incident.incident_type);
+          }
+        });
+        setIncidents(response.data);
+      })
+      .catch(error => 
+        console.error("Error fetching incident reports for map view:", error));
+  }, []);
 
-/*   return (
-//     <ChakraProvider>
-//       <Box position="relative" height="100vh" width="100%">
-//         <MapContainer center={[37.819, -122.478]} zoom={zoom} style={{ height: '100%', width: '100%' }} ref={mapRef}>
-//           <TileLayer
-//             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//           />
-//           <LocationMarker />
-//         </MapContainer>
-//         <Box position="absolute" right="20px" bottom="20px" width="200px" zIndex="10" bgColor="whiteAlpha.600" p="2" borderRadius="md">
-//         </Box>
-//       </Box>
-//     </ChakraProvider>
-//   );
-// }
-
-// export default MapPage;
-*/
-return (
-  <ChakraProvider>
-    <Box position="relative" height="100vh" width="100%">
-      <MapContainer center={[37.819, -122.478]} zoom={zoom} style={{ height: '100%', width: '100%' }} ref={mapRef}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <LocationMarker />
-        <SingleIconMarker incident={singleIcon} />
-        {
-        /*incidents.map(incident => (
-          <Marker
-            key={incident.incident_id}
-            position={[incident.location_lat, incident.location_lng]}
-            icon={mapIcons[incident.incident_type] || new L.Icon()}
-          >
-            <Popup>
-              <strong>{incident.incident_type}</strong>
-              <br />
-              {incident.description}
-              <br />
-              Reported at: {new Date(incident.timestamp).toLocaleString()}
-            </Popup>
-          </Marker>
-        ))*/}
-
-      </MapContainer>
-      <Box position="absolute" right="20px" bottom="20px" width="200px" zIndex="10" bgColor="whiteAlpha.600" p="2" borderRadius="md">
+  return (
+    <ChakraProvider>
+      <Box position="relative" height="100vh" width="100%">
+        <MapContainer center={[37.819, -122.478]} zoom={zoom} style={{ height: '100%', width: '100%' }} ref={mapRef}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <LocationMarker />
+          {incidents.map(incident => {
+            const iconType = normalizeIncidentType(incident.incident_type);
+            const icon = mapIcons[iconType] || new L.Icon({
+              iconUrl: 'path/to/default/icon.svg', // Specifying default icon (Need to do this later)
+              iconSize: [25, 41],
+              iconAnchor: [12, 41],
+              popupAnchor: [1, -34]
+            });
+            return (
+              <Marker
+                key={incident.incident_id}
+                position={[incident.location_lat, incident.location_lng]}
+                icon={icon}
+              >
+                <Popup>
+                  <strong>{incident.incident_type}</strong>
+                  <br />
+                  {incident.description}
+                  <br />
+                  Reported at: {new Date(incident.timestamp).toLocaleString()}
+                </Popup>
+              </Marker>
+            );
+          })}
+        </MapContainer>
+        <Box position="absolute" right="20px" bottom="20px" width="200px" zIndex="10" bgColor="whiteAlpha.600" p="2" borderRadius="md">
+        </Box>
       </Box>
-    </Box>
-  </ChakraProvider>
-);
+    </ChakraProvider>
+  );
 }
+
 
 export default MapPage;
