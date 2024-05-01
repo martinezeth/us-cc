9a85fc780b119c219368c2e2d7ca3eed365fed23
GGDoctor <127359046+GGDoctor@users.noreply.github.com>
2024-05-01 15:00:09 -0700

    Added links to ResourcePages

--- us-cc/src/Pages/ResourcesPage.js
+++ us-cc/src/Pages/ResourcesPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Text, Container, Heading, VStack, Link, Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon } from '@chakra-ui/react';
+import { Box, Text, Container, Heading, Link, Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon } from '@chakra-ui/react';
 
 function ResourcesPage() {
   return (
@@ -14,13 +14,13 @@ function ResourcesPage() {
           <h2>
             <AccordionButton>
               <Box flex="1" textAlign="left">
-                Natural Disasters
+                <b>Natural Disasters</b>
               </Box>
               <AccordionIcon />
             </AccordionButton>
           </h2>
           <AccordionPanel pb={4}>
-            Links and resources for dealing with floods, earthquakes, and more. <Link color="secondary.primary" href="#">FEMA</Link>
+            Links and resources for dealing with floods, earthquakes, and more. <Link color="secondary.primary" href="https://www.fema.gov/">FEMA</Link> or visit <Link color="secondary.primary" href="https://www.ready.gov/">Ready.gov</Link>
           </AccordionPanel>
         </AccordionItem>
         
@@ -28,27 +28,27 @@ function ResourcesPage() {
           <h2>
             <AccordionButton>
               <Box flex="1" textAlign="left">
-                Medical Emergencies
+                <b>Medical Emergencies</b>
               </Box>
               <AccordionIcon />
             </AccordionButton>
           </h2>
           <AccordionPanel pb={4}>
-            Resources for immediate medical assistance and health information. <Link color="secondary.primary" href="#">CDC Emergency</Link>
+            Resources for immediate medical assistance and health information. <Link color="secondary.primary" href="https://www.cdc.gov/">CDC Emergency</Link> or visit <Link color="secondary.primary" href="https://www.nih.gov/">National Institute of Health (NIH)</Link>
           </AccordionPanel>
         </AccordionItem>
 
         <AccordionItem>
           <h2>
             <AccordionButton>
-              <Box flex="1" textAlign="left">
-                Public Safety
+              <Box flex="1" textAlign="left" >
+                <b>Public Safety</b>
               </Box>
               <AccordionIcon />
             </AccordionButton>
           </h2>
           <AccordionPanel pb={4}>
-            Information on public safety measures and contacts. <Link color="secondary.primary" href="#">National Safety Council</Link>
+            Information on public safety measures and contacts. <Link color="secondary.primary" href="https://www.nsc.org/">National Safety Council</Link>
           </AccordionPanel>
         </AccordionItem>
       </Accordion>
