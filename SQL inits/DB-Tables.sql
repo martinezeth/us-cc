db424e5de8bc33dc04f739ce293537ec4ded9d6a
Nic Randazzo <nicmrandazzo@gmail.com>
2024-04-30 23:11:53 -0700

    made some db table changes, created VSP

--- SQL inits/DB-Tables.sql
+++ SQL inits/DB-Tables.sql
@@ -18,13 +18,14 @@ DROP TABLE IF EXISTS IncidentReports;
 
 DROP TABLE IF EXISTS IncidentType;
 
--- Create the Users table
 CREATE TABLE IF NOT EXISTS
     Users (
         user_id INT AUTO_INCREMENT PRIMARY KEY,
         username VARCHAR(50) NOT NULL UNIQUE,
         password_hash VARCHAR(255) NOT NULL,
+        name VARCHAR(255) NOT NULL,
         region VARCHAR(50) NULL,
+        date_joined DATE NULL,
         role ENUM('user', 'admin') DEFAULT 'user'
     );
 
@@ -34,10 +35,11 @@ CREATE TABLE IF NOT EXISTS
         user_id INT NOT NULL,
         title VARCHAR(255) NOT NULL,
         body TEXT NOT NULL,
+        region VARCHAR(50) NULL,
+        date_posted DATETIME NULL,
         FOREIGN KEY (user_id) REFERENCES Users (user_id)
     );
 
--- Create the IncidentReports table
 CREATE TABLE IF NOT EXISTS
     IncidentReports (
         incident_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -51,7 +53,7 @@ CREATE TABLE IF NOT EXISTS
         FOREIGN KEY (user_id) REFERENCES Users (user_id)
     );
 
--- Create the Resources table
+
 CREATE TABLE IF NOT EXISTS
     Resources (
         resource_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -61,7 +63,6 @@ CREATE TABLE IF NOT EXISTS
         location_lng DECIMAL(10, 6)
     );
 
--- Create the Volunteers table
 CREATE TABLE IF NOT EXISTS
     Volunteers (
         volunteer_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -73,19 +74,16 @@ CREATE TABLE IF NOT EXISTS
         FOREIGN KEY (user_id) REFERENCES Users (user_id)
     );
 
--- Create the Region table
 CREATE TABLE IF NOT EXISTS
     Region (
         region_id INT AUTO_INCREMENT PRIMARY KEY,
         region_name VARCHAR(100) NOT NULL,
-        state VARCHAR(100),
-        city VARCHAR(100),
-        postal_code VARCHAR(20),
-        country VARCHAR(100),
-        UNIQUE (region_name, country, state, city, postal_code)
+        state VARCHAR(100) NULL,
+        city VARCHAR(100) NULL,
+        postal_code VARCHAR(20) NULL,
+        UNIQUE (region_name, state, city, postal_code)
     );
 
--- Create the IncidentType table 
 CREATE TABLE IF NOT EXISTS
     IncidentType (
         incident_type_id INT AUTO_INCREMENT PRIMARY KEY,
