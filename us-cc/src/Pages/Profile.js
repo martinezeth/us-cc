56876b6b23254813d216e4948ebf45934c6f8f24
Nic Randazzo <nicmrandazzo@gmail.com>
2024-05-09 02:39:05 -0700

    no mo uscc

--- us-cc/src/Pages/Profile.js
+++ us-cc/src/Pages/Profile.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
-import { Avatar, Box, Text, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, Button, Input } from '@chakra-ui/react';
+import { Avatar, Flex, Box, Text, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, Button, Input } from '@chakra-ui/react';
 import '../Styles/profilecss.css';
 
 const EditProfileModal = ({ isOpen, onClose, userData, onUpdate }) => {
@@ -101,81 +101,153 @@ export default function Profile() {
     };
     
 
-
     return (
-        <Box
-            display="flex"
-            justifyContent="center"
-            height={'94vh'}
-            alignItems="center"
-            flexDirection="column"
-            backgroundColor="blue.200"
-            padding="20px"
-        >
-            <EditProfileModal
-                isOpen={isEditing}
-                onClose={() => setIsEditing(false)}
-                userData={userData || {}} 
-                onUpdate={handleUpdateProfile}
-            />
-            {userData ? (
-                <Box
-                    display="flex"
-                    justifyContent="center"
-                    alignItems="center"
-                    flexDirection="column"
-                >
-                    
+        <Flex justifyContent="center" alignItems="center" h="100vh">
+            <Box
+                minW="40%"
+                borderWidth="1px"
+                borderRadius="lg"
+                overflow="hidden"
+                p="8"
+                m="2"
+                boxShadow="lg"
+                bg="lightblue"
+            >
+                <EditProfileModal
+                    isOpen={isEditing}
+                    onClose={() => setIsEditing(false)}
+                    userData={userData || {}}
+                    onUpdate={handleUpdateProfile}
+                />
+                {userData ? (
                     <Box>
-                        <Avatar size="lg" />
+                        <Box display="flex" alignItems="center" justifyContent="center">
+                            <Avatar size="xl" />
+                        </Box>
+                        <Box textAlign="center" my="4">
+                            <Text fontSize="xl" fontWeight="bold">
+                                {userData.name || ""}
+                            </Text>
+                            <Text>{userData.username || 'Username'}</Text>
+                            <Text>{userData.role || 'Role'}</Text>
+                        </Box>
+                        <Box textAlign="center" my="4">
+                            <Text fontSize="lg" fontWeight="bold">
+                                Member since:{' '}
+                                {userData.date_joined
+                                    ? new Date(userData.date_joined).toLocaleDateString()
+                                    : 'Unknown join date'}
+                            </Text>
+                            {/* Display state and city if available */}
+                            {userData.region_id && (
+                                <>
+                                    <Text fontSize="md" fontWeight="bold">State: {userData.state || 'XXXXXXX'}</Text>
+                                    <Text fontSize="md" fontWeight="bold">City: {userData.city || 'XXXXXXX'}</Text>
+                                </>
+                            )}
+                        </Box>
+                        <Box textAlign="center" my="4">
+                            <Text fontSize="lg" fontWeight="bold">
+                                Currently volunteering at:
+                            </Text>
+                            {volunteering && volunteering.length > 0 ? (
+                                <Box as="ul" listStyleType="none" padding="0">
+                                    {volunteering[0].map((place, index) => (
+                                        <Text as="li" key={index}>
+                                            {place.location_name}
+                                        </Text>
+                                    ))}
+                                </Box>
+                            ) : (
+                                <Text>No volunteering places listed.</Text>
+                            )}
+                        </Box>
+                        <Box textAlign="center" my="4">
+                            <Button onClick={handleProfileEdit}>Edit Profile</Button>
+                        </Box>
                     </Box>
-                    <Box textAlign="center" marginY="10px">
-                        <Text fontSize="xl" fontWeight="bold">
-                             {userData.name || ""}
-                        </Text>
-                            {userData.username || 'Username'}
-                        
-                        <Text>{userData.role || 'Role'}</Text>
-                    </Box>
-                    <Box textAlign="center" marginY="10px">
-                        <Text fontSize="lg" fontWeight="bold">
-                            Member since:{' '}
-                            {userData.date_joined
-                                ? new Date(userData.date_joined).toLocaleDateString()
-                                : 'Unknown join date'}
-                        </Text>
-                        {/* Display state and city if available */}
-                        {userData.region_id && (
-                            <>
-                                <Text fontSize="md" fontWeight="bold">State: {userData.state || 'XXXXXXX'}</Text>
-                                <Text fontSize="md" fontWeight="bold">City: {userData.city || 'XXXXXXX'}</Text>
-                            </>
-                        )}
-                    </Box>
-                    <Box textAlign="center">
-                        <Text fontSize="lg" fontWeight="bold">
-                            Currently volunteering at:
-                        </Text>
-                        {volunteering && volunteering.length > 0 ? (
-                            <Box as="ul" listStyleType="none" padding="0">
-                                {volunteering[0].map((place, index) => (
-                                    <Text as="li" key={index}>
-                                        {place.location_name}
-                                    </Text>
-                                ))}
-                            </Box>
-                        ) : (
-                            <Text>No volunteering places listed.</Text>
-                        )}
-
-                    </Box>
-                    <Button onClick={handleProfileEdit}>Edit Profile</Button>
-                </Box>
-            ) : (
-                <Text fontSize="xl" fontWeight="bold">
-                    Unauthorized Access
-                </Text>
-            )}
-        </Box>
+                ) : (
+                    <Text fontSize="xl" fontWeight="bold">
+                        Unauthorized Access
+                    </Text>
+                )}
+            </Box>
+        </Flex>
     );
+    // return (
+    //     <Box
+    //         display="flex"
+    //         justifyContent="center"
+    //         height={'94vh'}
+    //         alignItems="center"
+    //         flexDirection="column"
+    //         backgroundColor="blue.200"
+    //         padding="20px"
+    //     >
+    //         <EditProfileModal
+    //             isOpen={isEditing}
+    //             onClose={() => setIsEditing(false)}
+    //             userData={userData || {}} 
+    //             onUpdate={handleUpdateProfile}
+    //         />
+    //         {userData ? (
+    //             <Box
+    //                 display="flex"
+    //                 justifyContent="center"
+    //                 alignItems="center"
+    //                 flexDirection="column"
+    //             >
+                    
+    //                 <Box>
+    //                     <Avatar size="lg" />
+    //                 </Box>
+    //                 <Box textAlign="center" marginY="10px">
+    //                     <Text fontSize="xl" fontWeight="bold">
+    //                          {userData.name || ""}
+    //                     </Text>
+    //                         {userData.username || 'Username'}
+                        
+    //                     <Text>{userData.role || 'Role'}</Text>
+    //                 </Box>
+    //                 <Box textAlign="center" marginY="10px">
+    //                     <Text fontSize="lg" fontWeight="bold">
+    //                         Member since:{' '}
+    //                         {userData.date_joined
+    //                             ? new Date(userData.date_joined).toLocaleDateString()
+    //                             : 'Unknown join date'}
+    //                     </Text>
+    //                     {/* Display state and city if available */}
+    //                     {userData.region_id && (
+    //                         <>
+    //                             <Text fontSize="md" fontWeight="bold">State: {userData.state || 'XXXXXXX'}</Text>
+    //                             <Text fontSize="md" fontWeight="bold">City: {userData.city || 'XXXXXXX'}</Text>
+    //                         </>
+    //                     )}
+    //                 </Box>
+    //                 <Box textAlign="center">
+    //                     <Text fontSize="lg" fontWeight="bold">
+    //                         Currently volunteering at:
+    //                     </Text>
+    //                     {volunteering && volunteering.length > 0 ? (
+    //                         <Box as="ul" listStyleType="none" padding="0">
+    //                             {volunteering[0].map((place, index) => (
+    //                                 <Text as="li" key={index}>
+    //                                     {place.location_name}
+    //                                 </Text>
+    //                             ))}
+    //                         </Box>
+    //                     ) : (
+    //                         <Text>No volunteering places listed.</Text>
+    //                     )}
+
+    //                 </Box>
+    //                 <Button onClick={handleProfileEdit}>Edit Profile</Button>
+    //             </Box>
+    //         ) : (
+    //             <Text fontSize="xl" fontWeight="bold">
+    //                 Unauthorized Access
+    //             </Text>
+    //         )}
+    //     </Box>
+    // );
 };
